64f6f8cf54b644440d595e7cd4f416235da02773
Jakub Plecháč <loxtox74@gmail.com>
2021-04-27 13:35:41 +0200

    finished Calculator app

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,7 +6,34 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter operator: ");
+            string operatorInput = Console.ReadLine();
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = default;
+            switch (operatorInput)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - +num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result is: {0}", result);
         }
     }
 }
